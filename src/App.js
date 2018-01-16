ec995881e3a9ca983487647c5a97c23ae0f69e89
Xander Gottlieb <alexander@alexandergottlieb.com>
2018-01-16 18:52:23 +0000

    Starting to route

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import React, { Component } from 'react';
+import { BrowserRouter, Route } from 'react-router-dom';
 import './App.css';
 import Editor from './components/Editor.js'
+import Sidebar from './components/Sidebar.js'
 
 class App extends Component {
   render() {
@@ -40,9 +42,21 @@ class App extends Component {
     };
 
     return (
-      <div className="App">
-        <Editor sentence={sentence} />
-      </div>
+      <BrowserRouter>
+        <div className="app">
+          <Route path="/edit">
+            <div class="route--edit">
+              <Sidebar />
+              <Editor sentence={sentence} />
+            </div>
+          </Route>
+          <Route path="/">
+            <div class="route--home">
+
+            </div>
+          </Route>
+        </div>
+      </BrowserRouter>
     );
   }
 }
---
+++ src/components/Sidebar.js
@@ -0,0 +1,27 @@
+import React, { Component } from 'react'
+
+class Sidebar extends Component {
+
+  constructor(props) {
+    super(props)
+
+    this.state = {}
+  }
+
+  render() {
+    return (
+      <div className="sidebar">
+        
+        <h1>Treebank Name</h1>
+        <input className="search" type="text" />
+        <div>
+
+        </div>
+        <button type="button">Download</button>
+        <button type="button">New Sentence</button>
+      </div>
+    )
+  }
+}
+
+export default Sidebar
