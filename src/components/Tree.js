ca7be3372397d195c5075b35aeaf767d2c163cd8
Xander Gottlieb <alexander@alexandergottlieb.com>
2018-01-26 13:18:31 +0000

    Colour scheme from alphabet research

--- src/components/Tree.js
+++ src/components/Tree.js
@@ -13,14 +13,14 @@ const Tree = props => {
     //Drawing calculations
     if (sentence) {
         //Scaling factors
-        const pixelUnit = 16
-        const yUnit = 8 * pixelUnit
+        const rem = parseFloat(window.getComputedStyle(document.body).getPropertyValue('font-size'))
+        const yUnit = 8 * rem
         let longestWord = 0
         for (let index in sentence.words) {
           if (sentence.words[index].inflection.length > longestWord) longestWord = sentence.words[index].inflection.length
         }
-        const wordWidth = longestWord * pixelUnit
-        const xUnit = wordWidth * 1.25 //relative to the longest word, scaled to add padding
+        const wordWidth = longestWord * rem
+        const xUnit = wordWidth * 1.33 //relative to the longest word, scaled to add padding
 
         //Calculate node positions
         let tree = new TreeDrawer(sentence)
@@ -37,18 +37,18 @@ const Tree = props => {
             let coords = {};
             //Line start co-ordinate
             coords.x1 = node.x * xUnit + (wordWidth/2);
-            coords.y1 = node.y * yUnit + pixelUnit;
+            coords.y1 = node.y * yUnit + rem;
             //Line end co-ordinate
             coords.x2 = child.x * xUnit + (wordWidth/2);
-            coords.y2 = child.y * yUnit + pixelUnit;
+            coords.y2 = child.y * yUnit + rem;
             const key = `${node.index}_${child.index}`
-            lines.push(<line {...coords} strokeWidth="2" stroke="#D2D7DA" key={key} />)
+            lines.push(<line {...coords} strokeWidth="2" stroke="#78797B" key={key} />)
             //Relation at midpoint of line
             const relationStyle = {
                 left: coords.x1 + ((coords.x2 - coords.x1) / 2) + 'px',
                 top: coords.y1 + ((coords.y2 - coords.y1) / 2) + 'px'
             }
-            relations.push(<span className="relation" style={relationStyle}>{child.word.relation}</span>)
+            relations.push(<span className="relation" style={relationStyle} key={child.index}>{child.word.relation}</span>)
           })
         })
     }
