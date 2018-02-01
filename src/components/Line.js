b1bbc0b4f748eaedfb9b034ee004d7aedbcbe0b9
Xander Gottlieb <alexander@alexandergottlieb.com>
2018-02-01 19:11:46 +0000

    Dragging lines works better but needs to be droppable

--- src/components/Line.js
+++ src/components/Line.js
@@ -1,35 +1,50 @@
 import React, { Component } from 'react'
 
-const Line = props => {
+class Line extends Component {
 
-    const { x1, x2, y1, y2, origin, active } = props
+    constructor(props) {
+        super(props)
 
-    let ref = null
-
-    //Follow mouse when active
-    if (active) {
-        let mouseX = x1
-        let mouseY = y1
-        console.log('mouseX init', mouseX)
-        console.log('mouseY init', mouseY)
-        document.body.addEventListener('mousemove', event => {
-            mouseX = event.clientX - origin.x
-            mouseY = event.clientY - origin.y
-        })
-        document.body.dispatchEvent(new Event('mousemove')) //Initialise position)
-
-        const animate = () => {
-            console.log('mouseX', mouseX)
-            console.log('mouseY', mouseY)
-            ref.setAttribute('x1', mouseX)
-            ref.setAttribute('y1', mouseY)
-            window.requestAnimationFrame(animate)
+        this.state = {
+            mouseX: 0,
+            mouseY: 0,
+            animationID: null
         }
-
-        window.requestAnimationFrame(animate)
     }
 
-    return <line x1={x1} x2={x2} y1={y1} y2={y2} strokeWidth="2" stroke="#78797B" ref={element => ref = element} />
+    componentWillReceiveProps(newProps) {
+        const { active, x1, y1, origin } = newProps
+
+        //Follow mouse when active
+        if (active) {
+            let mouseX = x1
+            let mouseY = y1
+            document.body.addEventListener('mousemove', event => {
+                mouseX = event.clientX - origin.x
+                mouseY = event.clientY - origin.y
+            })
+
+            const animate = () => {
+                console.log('animate')
+                this.setState({ mouseX, mouseY })
+                window.requestAnimationFrame(animate)
+            }
+
+            const animationID = requestAnimationFrame(animate)
+            this.setState({animationID})
+        } else {
+            cancelAnimationFrame(this.state.animationID)
+        }
+    }
+
+    render() {
+        const { x2, y2, active } = this.props
+
+        const x1 = active ? this.state.mouseX : this.props.x1
+        const y1 = active ? this.state.mouseY : this.props.y1
+
+        return <line x1={x1} x2={x2} y1={y1} y2={y2} strokeWidth="2" stroke="#78797B" />
+    }
 }
 
 export default Line;
