bca86fa8b08fddef311dea4310177a6e360e98cd
Xander Gottlieb <alexander@alexandergottlieb.com>
2018-03-26 16:32:19 +0200

    Added labels to arc view

--- src/components/Arc.js
+++ src/components/Arc.js
@@ -20,7 +20,9 @@ class Arc extends Component {
         if (depth > 5) depth = 5
         const y = depth * yUnit
 
-        //Draw arrows
+        //Draw arrows & labels
+        let relationLabels = []
+
         const arrows = sentence.words.map( word => {
             let { index, parent } = word
             if (parent === 0) return null //Skip root relation
@@ -33,15 +35,24 @@ class Arc extends Component {
             let x1 = index * scaling.units.x
             let x2 = parent * scaling.units.x + 0.5 * scaling.wordWidth
             //Finish on the parent side closest to child word
+            let labelPosition = ''
             if (distance > 0) {
+                labelPosition = 'right'
                 x1 += 0.66 * scaling.wordWidth
             } else {
+                labelPosition = 'left'
                 x1 += 0.33 * scaling.wordWidth
             }
+            //Add label
+            const labelStyle = {
+                top: `${y}px`,
+                left: `${x1}px`,
+            }
+            relationLabels .push(<span className={`arc__relation-label arc__relation-label--${labelPosition}`} style={labelStyle}>{treebank.settings.relations[word.relation]}</span>)
             //Calculate curve contorl point
             const cX = x1 + (0.5 * distance * scaling.wordWidth)
             let cY = Math.max(depth - Math.abs(distance), - depth/2) * yUnit
-            return <path d={`M${x1} ${y} Q${cX} ${cY} ${x2} ${y}`} key={word.id} strokeWidth="2" stroke="#758AA8" opacity="0.7" fill="transparent" markerStart="url(#arrow)" />
+            return <path d={`M${x1} ${y} Q${cX} ${cY} ${x2} ${y}`} key={word.id} strokeWidth="2" stroke="#758AA8" opacity="0.5" fill="transparent" markerStart="url(#arrow)" />
         })
 
         const words = sentence.words.map( word => {
@@ -71,6 +82,9 @@ class Arc extends Component {
                         {arrows}
                     </svg>
                     {words}
+                    <div className="arc__relation-labels">
+                        {relationLabels}
+                    </div>
                 </div>
             </div>
         )
